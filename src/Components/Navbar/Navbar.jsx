490ea0f82920d49175e3e278f66f5b06d1e24d29
NeptuneRjo <rjo9000@outlook.com>
2022-02-18 14:25:48 -0500

    jsx for navbar content written

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="space__navbar">
+      <div className="space__navbar-content">
+        <div className="space__navbar-nav nav-home">
+          <Link to='/'><p>00</p>Home</Link>
+        </div>
+        <div className="space__navbar-nav nav-destination">
+          <Link to='/destination'><p>01</p>Destination</Link>
+        </div>
+        <div className="space__navbar-nav nav-crew">
+          <Link to='/crew'><p>02</p>Crew</Link>
+        </div>
+        <div className="space__navbar-nav nav-technology">
+          <Link to='/technology'><p>03</p>Technology</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
