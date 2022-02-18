80f5fed6759b10010d07e594e2952e6c9a48acfa
NeptuneRjo <rjo9000@outlook.com>
2022-02-18 18:49:20 -0500

    added jsx for home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,7 +9,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="space__home"></div>
+    <div className="space__home">
+      <div className="space__home-container">
+        <div className="space__home-content">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        </div>
+        <div className="space__home-explore">
+        <div className="space__home-explore-button">EXPLORE</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
