c7019de21f5fad5537b2959ed74a62881689e563
NeptuneRjo <rjo9000@outlook.com>
2022-02-20 15:51:34 -0500

    added jsx and setup handleClick function

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -1,40 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import handleNav from '../Navbar/handleNav';
+import data from '../../data.json';
 import './destination.css';
-import data from '../../data.json'
+import { moonImage, titanImage, marsImage, europaImage } from '../../Assets/destination/index'; // Images
 
 
 const Destination = () => {
 
   const destination = data.destinations;
-  const index = 0;
-  console.log(destination)
+  const [index, setIndex] = useState(0);
+
+  // const handleClick = (selectedDestination) => {
+  //   const navs = document.querySelectorAll('')
+  //   const navContainer = document.querySelector('')
+
+  //   for (let i = 0; i < navs.length; i++) {
+  //     navs[i].classList.remove('selected-nav')
+  //   }
+
+  //   selectedNav.classList.add('selected-nav')
+  // }
 
   useEffect(() => {
     handleNav(document.querySelector('.nav-destination'))
   }, []);
 
   return (
-    <div className="destination-main">
+    <div className='destination-main'>
       <div className="destination-header">
         <p>01</p>PICK YOUR DESTINATION</div>
       <div className="destination-container">
-        <div className="destination-image">
-          <img src='#' alt='#' />
-        </div>
+        <div className="destination-image"><img src={moonImage}/></div>
         <div className="destination-content">
-          <div className="destination-nav"></div>
-          <h2>{destination[index].name}</h2>
-          <p>{destination[index].description}</p>
+          <div className="destination-nav">
+            <div className="destination-nav-item nav-moon">MOON</div>
+            <div className="destination-nav-item nav-mars">MARS</div>
+            <div className="destination-nav-item nav-europa">EUROPA</div>
+            <div className="destination-nav-item nav-titan">TITAN</div>
+          </div>
+          <h2 className="destination-title">{destination[index].name}</h2>
+          <p className="destination-text">{destination[index].description}</p>
           <div className="destination-stats">
-            <div className="destination-states">
-              <div className="destination-distance"></div>
-              <p></p>
-              <h5></h5>
+            <div className="destination-distance">
+              <div>AVG. DISTANCE</div>
+              <p>{destination[index].distance}</p>
             </div>
             <div className="destination-time">
-              <p></p>
-              <h5></h5>
+              <div>EST. TRAVEL TIME</div>
+              <p>{destination[index].travel}</p>
             </div>
           </div>
         </div>
