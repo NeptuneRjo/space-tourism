5bac47c256fc40475e5488ab2e85e23ef0615fd7
NeptuneRjo <rjo9000@outlook.com>
2022-02-20 15:53:41 -0500

    consolodated image imports into one file for easier import later on

---
+++ src/Assets/destination/index.js
@@ -0,0 +1,11 @@
+import moonImage from './image-moon.png';
+import europaImage from './image-europa.png';
+import marsImage from './image-mars.png';
+import titanImage from './image-titan.png';
+
+export {
+    moonImage,
+    europaImage,
+    marsImage,
+    titanImage
+}
