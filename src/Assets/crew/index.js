b5784a3d4ba2d535b9a3450b8e7656d361a7c2ac
NeptuneRjo <rjo9000@outlook.com>
2022-02-20 17:30:35 -0500

    consolodated image exports for easier access

---
+++ src/Assets/crew/index.js
@@ -0,0 +1,11 @@
+import hurley from './image-douglas-hurley.png';
+import shuttlesworth from './image-mark-shutttlesworth.png';
+import glover from './image-victor-glover.png';
+import ansari from './image-anousheh-ansari.png';
+
+export {
+    hurley,
+    shuttlesworth,
+    glover,
+    ansari
+}
