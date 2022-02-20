dc3b86a18a1b011a3e165312cb2d1b39e5cd9d7c
NeptuneRjo <rjo9000@outlook.com>
2022-02-20 15:52:03 -0500

    cleaned up handleNav

--- src/Components/Navbar/handleNav.js
+++ src/Components/Navbar/handleNav.js
@@ -1,7 +1,6 @@
 
 const handleNav = (selectedNav) => {
     const navs = document.querySelectorAll('.space__navbar-nav');
-    const navContainer = document.querySelector('.space__navbar')
 
     for (let i = 0; i < navs.length; i++) {
       navs[i].classList.remove('selected__nav')
