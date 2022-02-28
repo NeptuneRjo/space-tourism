84d2c39760fa1562a035a316707850dc55785062
NeptuneRjo <rjo9000@outlook.com>
2022-02-28 00:29:08 -0500

    jsx for notfound page written

--- src/Components/Notfound/Notfound.jsx
+++ src/Components/Notfound/Notfound.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './notfound.css';
 
 export default class Notfound extends Component {
   render() {
     return (
-      <div>Notfound</div>
+      <div className="notfound-main">
+        <h1 className="notfound-header">Error! This page does not exist.</h1>
+        <Link to='/'>Try this page instead...</Link>
+      </div>
     )
   }
 }
