e2697114346ec749819e3101cc31212fe2b5e12c
NeptuneRjo <rjo9000@outlook.com>
2022-09-01 17:23:53 -0400

    refactor navbar code; reduced by 50 lines; now a reusable component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,34 +3,22 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ navItems }) => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="space__navbar">
-      <div className="space__navbar-content">
-        <div className="space__navbar-nav nav-home">
-          <Link to="/">
-            <p>00</p>Home
-          </Link>
-        </div>
-        <div className="space__navbar-nav nav-destination">
-          <Link to="/destination">
-            <p>01</p>Destination
-          </Link>
-        </div>
-        <div className="space__navbar-nav nav-crew">
-          <Link to="/crew">
-            <p>02</p>Crew
-          </Link>
-        </div>
-        <div className="space__navbar-nav nav-technology">
-          <Link to="/technology">
-            <p>03</p>Technology
-          </Link>
-        </div>
+    <div className="navbar">
+      <div className="navbar-desktop">
+        {navItems.map((item) => (
+          <div className="navbar-item">
+            <Link to={`/${item.txt}`}>
+              <span>{item.num}</span>
+              {item.txt}
+            </Link>
+          </div>
+        ))}
       </div>
-      <div className="space__navbar-content-menu">
+      <div className="navbar-mobile">
         {toggleMenu ? (
           <RiCloseLine
             color="#fff"
@@ -44,32 +32,80 @@ const Navbar = () => {
             onClick={() => setToggleMenu(true)}
           />
         )}
-        {toggleMenu && (
-          <div className="space__navbar-content-menu slide-out">
-            <div className="space__navbar-nav nav-home">
+        {toggleMenu &&
+          navItems.map((item) => (
+            <div className="navbar-item">
               <Link to="/">
-                <p>00</p>Home
+                <span>{item.num}</span>
+                {item.txt.toUpperCase()}
               </Link>
             </div>
-            <div className="space__navbar-nav nav-destination">
-              <Link to="/destination">
-                <p>01</p>Destination
-              </Link>
-            </div>
-            <div className="space__navbar-nav nav-crew">
-              <Link to="/crew">
-                <p>02</p>Crew
-              </Link>
-            </div>
-            <div className="space__navbar-nav nav-technology">
-              <Link to="/technology">
-                <p>03</p>Technology
-              </Link>
-            </div>
-          </div>
-        )}
+          ))}
       </div>
     </div>
+    // <div className="space__navbar">
+    //   <div className="space__navbar-content">
+    //     <div className="space__navbar-nav nav-home">
+    //       <Link to="/">
+    //         <p>00</p>Home
+    //       </Link>
+    //     </div>
+    //     <div className="space__navbar-nav nav-destination">
+    //       <Link to="/destination">
+    //         <p>01</p>Destination
+    //       </Link>
+    //     </div>
+    //     <div className="space__navbar-nav nav-crew">
+    //       <Link to="/crew">
+    //         <p>02</p>Crew
+    //       </Link>
+    //     </div>
+    //     <div className="space__navbar-nav nav-technology">
+    //       <Link to="/technology">
+    //         <p>03</p>Technology
+    //       </Link>
+    //     </div>
+    //   </div>
+    //   <div className="space__navbar-content-menu">
+    // {toggleMenu ? (
+    //   <RiCloseLine
+    //     color="#fff"
+    //     size={27}
+    //     onClick={() => setToggleMenu(false)}
+    //   />
+    // ) : (
+    //   <RiMenu3Line
+    //     color="#fff"
+    //     size={27}
+    //     onClick={() => setToggleMenu(true)}
+    //   />
+    // )}
+    //     {toggleMenu && (
+    //       <div className="space__navbar-content-menu slide-out">
+    //         <div className="space__navbar-nav nav-home">
+    //           <Link to="/">
+    //             <p>00</p>Home
+    //           </Link>
+    //         </div>
+    //         <div className="space__navbar-nav nav-destination">
+    //           <Link to="/destination">
+    //             <p>01</p>Destination
+    //           </Link>
+    //         </div>
+    //         <div className="space__navbar-nav nav-crew">
+    //           <Link to="/crew">
+    //             <p>02</p>Crew
+    //           </Link>
+    //         </div>
+    //         <div className="space__navbar-nav nav-technology">
+    //           <Link to="/technology">
+    //             <p>03</p>Technology
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 
