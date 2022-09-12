a15f4d2b074bee451e22832f2a03d86798ccf97d
NeptuneRjo <rjo9000@outlook.com>
2022-09-12 17:41:08 -0400

    add crew member component that dynamically fills in content using an array of crew members

--- src/Components/CrewMember/CrewMember.jsx
+++ src/Components/CrewMember/CrewMember.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const CrewMember = () => {
-  return <div>CrewMember</div>;
+const CrewMember = ({ crew }) => {
+  const [crewIndex, setCrewIndex] = useState(0);
+  const crewMember = crew[crewIndex];
+
+  return (
+    <div className="crew_member-main">
+      <div className="crew_member-image">
+        <img
+          src={`${crewMember.images.png}`}
+          alt={`Image of ${crewMember.name}`}
+        />
+      </div>
+      <div className="crew_member-content">
+        <div className="crew_member-slider">
+          {crew.map((member, index) => (
+            <div
+              className={`slider-1 ${crewIndex === index ? "active" : ""}`}
+              onClick={() => setCrewIndex(index)}
+            />
+          ))}
+        </div>
+        <div className="crew_member-body">
+          <div className="crew_member-role heading-4">{crewMember.role}</div>
+          <div className="crew_member-name heading-3">{crewMember.name}</div>
+          <div className="crew_member-bio body-text">{crewMember.bio}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CrewMember;
