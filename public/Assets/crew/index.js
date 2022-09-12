9abeb24fc576f12d93c4c2dbcd13849144980bad
NeptuneRjo <rjo9000@outlook.com>
2022-09-12 17:41:21 -0400

    add assets to public folder

---
+++ public/Assets/crew/index.js
@@ -0,0 +1,6 @@
+import hurley from "./image-douglas-hurley.png";
+import shuttleworth from "./image-mark-shuttleworth.png";
+import glover from "./image-victor-glover.png";
+import ansari from "./image-anousheh-ansari.png";
+
+export { hurley, shuttleworth, glover, ansari };
