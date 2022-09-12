a5bde4529c55cc28dc6d114bf8bd9936a49440e0
NeptuneRjo <rjo9000@outlook.com>
2022-09-12 17:41:45 -0400

    add and implement crew member component

--- src/Pages/Crew/Crew.jsx
+++ src/Pages/Crew/Crew.jsx
@@ -2,73 +2,17 @@ import React, { useEffect, useState } from "react";
 import { handleNav, CrewMember } from "../../Components";
 import data from "../../data.json";
 import "./crew.css";
-import { hurley, ansari, shuttleworth, glover } from "../../Assets/crew/index";
 
-const Crew = ({ handleClick, crewMembers }) => {
-  const items = document.getElementsByClassName("crew-nav-member");
-
-  const [index, setIndex] = useState(0);
-  const [image, setImage] = useState(hurley);
-
-  useEffect(() => {
-    handleNav(document.querySelector(".nav-crew"));
-    handleClick(
-      items,
-      "selected-member",
-      document.querySelector(".nav-hurley")
-    );
-  }, []);
+const Crew = () => {
+  const { crew } = data;
 
   return (
     <div className="crew-main">
-      <div className="crew-header">
-        <p>02</p>MEET YOUR CREW
+      <div className="crew heading-5">
+        <span>02</span>MEET YOUR CREW
       </div>
       <div className="crew-container">
-        {/* <div className="crew-image">
-          <img src={image} />
-        </div>
-        <div className="crew-content">
-          <div className="crew-nav">
-            <div
-              className="crew-nav-member nav-hurley"
-              onClick={(e) => {
-                handleClick(items, "selected-member", e.target);
-                setIndex(0);
-                setImage(hurley);
-              }}
-            ></div>
-            <div
-              className="crew-nav-member nav-shuttleworth"
-              onClick={(e) => {
-                handleClick(items, "selected-member", e.target);
-                setIndex(1);
-                setImage(shuttleworth);
-              }}
-            ></div>
-            <div
-              className="crew-nav-member nav-glover"
-              onClick={(e) => {
-                handleClick(items, "selected-member", e.target);
-                setIndex(2);
-                setImage(glover);
-              }}
-            ></div>
-            <div
-              className="crew-nav-member nav-ansari"
-              onClick={(e) => {
-                handleClick(items, "selected-member", e.target);
-                setIndex(3);
-                setImage(ansari);
-              }}
-            ></div>
-          </div>
-          <div className="crew-info">
-            <h4 className="crew-role">{crew[index].role}</h4>
-            <h3 className="crew-name">{crew[index].name}</h3>
-            <div className="crew-description">{crew[index].bio}</div>
-          </div>
-        </div> */}
+        <CrewMember crew={crew} />
       </div>
     </div>
   );
