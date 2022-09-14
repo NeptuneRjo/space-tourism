63f5e57d6675daeaced6c82be18f4e2d53f08e45
NeptuneRjo <rjo9000@outlook.com>
2022-09-13 22:20:54 -0400

    add destination item to exports

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as handleNav } from "./Navbar/handleNav";
 export { default as CrewMember } from "./CrewMember/CrewMember";
+export { default as DestinationItem } from "./DestinationItem/DestinationItem";
