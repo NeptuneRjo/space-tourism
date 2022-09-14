a23cc9599ae5e13187bcdc7aadd4923d6dd9cb30
NeptuneRjo <rjo9000@outlook.com>
2022-09-13 23:23:26 -0400

    add and implement tech item component

--- src/Pages/Technology/Technology.jsx
+++ src/Pages/Technology/Technology.jsx
@@ -1,82 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { handleNav } from "../../Components";
+import React from "react";
 import "./technology.css";
 import data from "../../data.json";
+import TechItem from "../../Components/TechItem/TechItem";
 
-const Technology = ({ handleClick }) => {
-  const technology = data.technology;
-
-  const items = document.getElementsByClassName("technology-nav-item");
-
-  const [index, setIndex] = useState(0);
-  const [imageClass, setImageClass] = useState("launchVehicle");
-
-  // const handleClick = (currentTech) => {
-  //   const navs = document.querySelectorAll(".technology-nav-item");
-  //   const image = document.querySelector(".technology-image");
-
-  //   for (let i = 0; i < navs.length; i++) {
-  //     navs[i].classList.remove("selected-tech");
-  //   }
-
-  //   currentTech.classList.add("selected-tech");
-  // };
-
-  useEffect(() => {
-    handleNav(document.querySelector(".nav-technology"));
-    handleClick(items, "selected-tech", document.querySelector(".nav1"));
-  }, []);
+const Technology = () => {
+  const { technology } = data;
 
   return (
     <div className="technology-main">
       <div className="technology-header">
         <p>03</p>SPACE LAUNCH 101
       </div>
-      <div className="technology-container">
-        <div className="technology-image">
-          <img className={imageClass} />
-        </div>
-        <div className="technology-content">
-          <div className="technology-nav">
-            <div
-              className="technology-nav-item nav1"
-              onClick={(e) => {
-                handleClick(items, "selected-tech", e.target);
-                setIndex(0);
-                setImageClass("launchVehicle");
-              }}
-            >
-              1
-            </div>
-            <div
-              className="technology-nav-item nav2"
-              onClick={(e) => {
-                handleClick(items, "selected-tech", e.target);
-                setIndex(1);
-                setImageClass("spacePort");
-              }}
-            >
-              2
-            </div>
-            <div
-              className="technology-nav-item nav3"
-              onClick={(e) => {
-                handleClick(items, "selected-tech", e.target);
-                setIndex(2);
-                setImageClass("spaceCapsule");
-              }}
-            >
-              3
-            </div>
-          </div>
-          <div className="technology-info">
-            <div className="technology-subheader">THE TERMINOLOGY...</div>
-            <h3 className="technology-title">{technology[index].name}</h3>
-            <p className="technology-description">
-              {technology[index].description}
-            </p>
-          </div>
-        </div>
+      <div className="container">
+        <TechItem technologies={technology} />
       </div>
     </div>
   );
