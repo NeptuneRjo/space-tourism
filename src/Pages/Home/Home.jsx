d3c1a72777f5fff034a09d54ac93c6482a953160
NeptuneRjo <rjo9000@outlook.com>
2022-09-13 22:33:00 -0400

    update class names

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect } from "react";
-import { handleNav } from "../../Components";
+import React from "react";
 import "./home.css";
 
 const Home = () => {
-  useEffect(() => {
-    handleNav(document.querySelector(".nav-home"));
-  }, []);
-
   return (
-    <div className="space__home">
-      <div className="space__home-container">
-        <div className="space__home-content">
-          <h5>SO, YOU WANT TO TRAVEL TO</h5>
-          <h1>SPACE</h1>
-          <p>
+    <div className="space-main">
+      <div className="space-container">
+        <div className="space-content">
+          <div className="heading-5">SO, YOU WANT TO TRAVEL TO</div>
+          <div className="heading-1">SPACE</div>
+          <div className="body-text">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we'll give you a truly out of this
             world experience!
-          </p>
+          </div>
         </div>
-        <div className="space__home-explore">
-          <div className="space__home-explore-button">EXPLORE</div>
+        <div className="space-explore">
+          <div className="space-explore-button">EXPLORE</div>
         </div>
       </div>
     </div>
