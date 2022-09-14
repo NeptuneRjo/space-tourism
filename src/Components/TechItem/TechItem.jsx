8e6510a35da66dfa9cfeaa72c97660b0f4f43515
NeptuneRjo <rjo9000@outlook.com>
2022-09-14 02:28:57 -0400

    format

--- src/Components/TechItem/TechItem.jsx
+++ src/Components/TechItem/TechItem.jsx
@@ -1,47 +1,47 @@
-import React, { useState } from "react";
-import "./style.css";
+import React, { useState } from 'react'
+import './style.css'
 
 const TechItem = ({ technologies }) => {
-  const [techIndex, setTechIndex] = useState(0);
-  const technology = technologies[techIndex];
+	const [techIndex, setTechIndex] = useState(0)
+	const technology = technologies[techIndex]
 
-  return (
-    <div className="tech_item-main">
-      <div className="tech_item-image">
-        <img
-          className="tech_item-image-landscape"
-          src={`${technology.images.landscape}`}
-          alt={`Image of ${technology.name}`}
-        />
-        <img
-          className="tech_item-image-portrait"
-          src={`${technology.images.portrait}`}
-          alt={`Image of ${technology.name}`}
-        />
-      </div>
-      <div className="tech_item-content">
-        <div className="tech_item-slider">
-          {technologies.map((tech, index) => (
-            <div
-              className={`slider-2 ${techIndex === index ? "active" : ""}`}
-              onClick={() => setTechIndex(index)}
-            >
-              {index + 1}
-            </div>
-          ))}
-        </div>
-        <div className="tech_item-body">
-          <div className="tech_item-title subheading-2">THE TERMINOLOGY...</div>
-          <div className="tech_item-name heading-3">
-            {technology.name.toUpperCase()}
-          </div>
-          <div className="tech_item-body body-text">
-            {technology.description}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='tech_item-main'>
+			<div className='tech_item-image'>
+				<img
+					className='tech_item-image-landscape'
+					src={`${technology.images.landscape}`}
+					alt={`Image of ${technology.name}`}
+				/>
+				<img
+					className='tech_item-image-portrait'
+					src={`${technology.images.portrait}`}
+					alt={`Image of ${technology.name}`}
+				/>
+			</div>
+			<div className='tech_item-content'>
+				<div className='tech_item-slider'>
+					{technologies.map((tech, index) => (
+						<div
+							className={`slider-2 ${techIndex === index ? 'active' : ''}`}
+							onClick={() => setTechIndex(index)}
+						>
+							{index + 1}
+						</div>
+					))}
+				</div>
+				<div className='tech_item-body'>
+					<div className='tech_item-title subheading-2'>THE TERMINOLOGY...</div>
+					<div className='tech_item-name heading-3'>
+						{technology.name.toUpperCase()}
+					</div>
+					<div className='tech_item-body body-text'>
+						{technology.description}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default TechItem;
+export default TechItem
