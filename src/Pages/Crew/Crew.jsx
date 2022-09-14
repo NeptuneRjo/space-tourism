2ef15e4b17c8511d074b871346451911facf5d4d
NeptuneRjo <rjo9000@outlook.com>
2022-09-14 19:28:23 -0400

    change container className

--- src/Pages/Crew/Crew.jsx
+++ src/Pages/Crew/Crew.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { handleNav, CrewMember } from "../../Components";
-import data from "../../data.json";
-import "./crew.css";
+import React, { useEffect, useState } from 'react'
+import { handleNav, CrewMember } from '../../Components'
+import data from '../../data.json'
+import './crew.css'
 
 const Crew = () => {
-  const { crew } = data;
+	const { crew } = data
 
-  return (
-    <div className="crew-main">
-      <div className="crew heading-5">
-        <span>02</span>MEET YOUR CREW
-      </div>
-      <div className="container">
-        <CrewMember crew={crew} />
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='crew-main'>
+			<div className='crew heading-5'>
+				<span>02</span>MEET YOUR CREW
+			</div>
+			<div className='crew-container'>
+				<CrewMember crew={crew} />
+			</div>
+		</div>
+	)
+}
 
-export default Crew;
+export default Crew
