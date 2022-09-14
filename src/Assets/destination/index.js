9a16c0dc990dd861fc9a74d0f574753d487fa8e1
NeptuneRjo <rjo9000@outlook.com>
2022-09-14 19:36:25 -0400

    fix spacing differences between text container and image container

--- src/Assets/destination/index.js
+++
@@ -1,6 +0,0 @@
-import moonImage from "./image-moon.png";
-import europaImage from "./image-europa.png";
-import marsImage from "./image-mars.png";
-import titanImage from "./image-titan.png";
-
-export { moonImage, europaImage, marsImage, titanImage };
