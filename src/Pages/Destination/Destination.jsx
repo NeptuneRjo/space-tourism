d62999fa1cbce61dccb5a2477ae97ae3dd2a960b
NeptuneRjo <rjo9000@outlook.com>
2022-09-14 22:59:30 -0400

    add responsive styling for 768px

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import data from "../../data.json";
-import "./destination.css";
-import { DestinationItem } from "../../Components";
+import React from 'react'
+import data from '../../data.json'
+import './destination.css'
+import { DestinationItem } from '../../Components'
 
 const Destination = ({}) => {
-  const { destinations } = data;
+	const { destinations } = data
 
-  return (
-    <div className="destination-main">
-      <div className="destination heading-5">
-        <span>01</span>PICK YOUR DESTINATION
-      </div>
-      <div className="container">
-        <DestinationItem destinations={destinations} />
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='destination-main'>
+			<div className='destination heading-5'>
+				<span>01</span>PICK YOUR DESTINATION
+			</div>
+			<div className='destination-container'>
+				<DestinationItem destinations={destinations} />
+			</div>
+		</div>
+	)
+}
 
-export default Destination;
+export default Destination
