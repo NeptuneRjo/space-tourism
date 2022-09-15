430fe102d25e9a07716e02c982ec747ec83f815b
NeptuneRjo <rjo9000@outlook.com>
2022-09-14 23:15:01 -0400

    add responsive styling for up to 768px

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,28 @@
-import React from "react";
-import "./home.css";
+import React from 'react'
+import './home.css'
 
 const Home = () => {
-  return (
-    <div className="space-main">
-      <div className="space-container">
-        <div className="space-content">
-          <div className="heading-5">SO, YOU WANT TO TRAVEL TO</div>
-          <div className="heading-1">SPACE</div>
-          <div className="body-text">
-            Let's face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we'll give you a truly out of this
-            world experience!
-          </div>
-        </div>
-        <div className="space-explore">
-          <div className="space-explore-button">EXPLORE</div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='space-main'>
+			<div className='space-container'>
+				<div className='space-content'>
+					<div className='space-heading-5 heading-5'>
+						SO, YOU WANT TO TRAVEL TO
+					</div>
+					<div className='space-heading-1 heading-1'>SPACE</div>
+					<div className='body-text'>
+						Let's face it; if you want to go to space, you might as well
+						genuinely go to outer space and not hover kind of on the edge of it.
+						Well sit back, and relax because we'll give you a truly out of this
+						world experience!
+					</div>
+				</div>
+				<div className='space-explore'>
+					<div className='space-explore-button'>EXPLORE</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
