9541d108221de904f38146c6d822257e7e9a3e54
NeptuneRjo <rjo9000@outlook.com>
2022-09-18 21:43:47 -0400

    add responsive styling for home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 						SO, YOU WANT TO TRAVEL TO
 					</div>
 					<div className='space-heading-1 heading-1'>SPACE</div>
-					<div className='body-text'>
+					<div className='space-body body-text'>
 						Let's face it; if you want to go to space, you might as well
 						genuinely go to outer space and not hover kind of on the edge of it.
 						Well sit back, and relax because we'll give you a truly out of this
@@ -18,7 +18,7 @@ const Home = () => {
 					</div>
 				</div>
 				<div className='space-explore'>
-					<div className='space-explore-button'>EXPLORE</div>
+					<div className='space-explore-button heading-4'>EXPLORE</div>
 				</div>
 			</div>
 		</div>
